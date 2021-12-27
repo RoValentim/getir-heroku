e6cfd15437f2d2e53010283c8c0574cff8d7b23a
RoValentim <rodrigo.do.tango@gmail.com>
2021-12-27 09:51:01 +0100

    Fixed port number

--- restful.go
+++ restful.go
@@ -18,5 +18,5 @@ func main() {
 	r.Route(http.MethodGet,  `/v1/memory`,   memory.GetData   )
 	r.Route(http.MethodPost, `/v1/memory`,   memory.CreateData)
 
-        http.ListenAndServe(":8080", r)
+        http.ListenAndServe(":443", r)
 }
