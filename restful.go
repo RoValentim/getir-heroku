af6ba18c125d88e083fc46734f0f376a81780e9c
RoValentim <rodrigo.do.tango@gmail.com>
2021-12-27 09:41:19 +0100

    Fixed port number

--- restful.go
+++ restful.go
@@ -10,9 +10,13 @@ import (
 func main() {
 	r := &router.Router{}
 
+	r.Route(http.MethodGet, "/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Working!"))
+	})
+
 	r.Route(http.MethodPost, `/v1/database`, database.GetData )
 	r.Route(http.MethodGet,  `/v1/memory`,   memory.GetData   )
 	r.Route(http.MethodPost, `/v1/memory`,   memory.CreateData)
 
-        http.ListenAndServe(":80", r)
+        http.ListenAndServe(":", r)
 }
